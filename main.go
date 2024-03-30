461f6507d1a4250c9ca2c47b4423168a8ce510c3
br0steen <osteensco@gmail.com>
2024-03-29 23:03:58 -0500

    main implementation

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
 )
 
 
-func passCmd(args []string) []string {
-    
-    if (len(args)) >= 2 {
-        return args[1:]
-    } else {
-        fmt.Println("No command provided. Usage: ftravel <command> <path>)
-        os.Exit(1)
+func passCmd(args []string) ([]string, error) {
+
+    if len(args) <= 2 {
+        return nil, errors.New("Insufficient args provided, usage: ftrav <command> <path/key>")
     }
+    return args[1:], nil
 
 }
 
-func changeDirectory(path string) error {
+
+func readMap(jsonPath string) map[string]string {
+    file, err := os.Open(jsonPath)
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }                                
     
+    defer file.Close()
+    
+    var pathMap map[string]string
+    decoder := json.NewDecoder(file)
+    if err := decoder.Decode(&pathMap); err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+
+    return pathMap
+
+}
+
+
+func changeDirectory(cmd []string, allPaths map[string]string, jsonPath string) {
+
+    path := allPaths[cmd[1]]
+
     err := os.Chdir(path)
     if err != nil {
         fmt.Printf("Fast travel failed! %v", err)
@@ -29,25 +53,73 @@ func changeDirectory(path string) error {
 
 }
 
+func setDirectoryVar(cmd []string, allPaths map[string]string, jsonPath string) {
+     
+    path, err := os.Getwd()
+    if err != nil {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
+    allPaths[cmd[2]] = path
+    
+    jsonData, err := json.MarshalIndent(allPaths, "", "  ")
+    if err != nil {
+        fmt.Println("Error marshalling JSON:", err)
+        os.Exit(1)
+    }
+ 
+    file, err := os.Create(jsonPath)
+    if err != nil {
+        fmt.Println("Error creating file:", err)
+        os.Exit(1)
+    }
+
+    defer file.Close()
+   
+    _, err = file.Write(jsonData)
+    if err != nil {
+        fmt.Println("Error writing JSON to file:", err)
+        os.Exit(1)                                                                                                        
+    }
+}
+
+// func displayAllPaths(cmd []string, allPaths map[string]string, jsonPath string) {}
+// type cmdArgs struct {
+//     cmd []string 
+//     allPaths map[string]string  
+//     jsonPath string 
+// }
 
 func main() {
     
-    availCmds := map[string]func(path string) {
+    // read in json file
+    jsonPath := "fastTravel.json"
+    allPaths := readMap(jsonPath)
+
+    // map available ftrav commands 
+    availCmds := map[string]func(cmd []string, allPaths map[string]string, jsonPath string) {
         "to": changeDirectory,
+        "set": setDirectoryVar,
+        // "ls": displayAllPaths
     }
     
-    inputCommand := passCmd(os.Args)
-    action := input_command[0]
-    path := input_command[1]
+    // sanitize input
+    inputCommand, err := passCmd(os.Args)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    action := inputCommand[0]
 
-    
+
+    // execute user provided action
     exeCmd, ok := availCmds[action]
     if !ok {
         fmt.Println("Invalid command, use 'help' for available commands.")
         os.Exit(1)
     }
 
-    exeCmd(path)
+    exeCmd(inputCommand, allPaths, jsonPath)
 
 }
 
