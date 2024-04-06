4010a402807a8e8e4aebae7a3b4bc954e1666c32
br0steen <osteensco@gmail.com>
2024-04-05 21:57:34 -0500

    init setup and concept map

---
+++ install.sh
@@ -0,0 +1,22 @@
+
+
+
+SCRIPT_PATH="./main.sh"
+
+
+
+function find_target_dir {}
+
+
+TARGET_DIR=""
+USER_SHELL=""
+
+# based on user's os, determine correct directory for exe to save to
+# prompt user to pick supported shell
+# add main.sh contents to users shell rc 
+# test executable by running `version` command (ps, add `version` command) 
+
+
+
+
+
