129542b101b03d3b650aec0a54ecd3c1302dd878
Scott Osteen <osteensco@gmail.com>
2025-01-29 19:30:07 -0600

    added cli wrapper script

--- shells/tests/cli/bash/sudo.sh
+++ shells/tests/cli/bash/sudo.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Used to spoof sudo.
+# Docker typically runs as root, so trying to use sudo throws an error.
+# Hence, we must spoof.
+
 sudo() {
     eval "$@"
 }
--- shells/tests/cli/test.sh
+++ shells/tests/cli/test.sh
@@ -5,4 +5,38 @@
 
 
 
+set -eo pipefail
+
+declare -A TESTS=(
+  ["./tests/bash/bash_test.sh"]="/bin/bash"
+  # ["./tests/zsh/zsh_test.sh"]="/bin/zsh"
+)
+
+all_tests_passed=true
+
+for TEST in "${!TESTS[@]}"; do
+    SHELL="${TESTS[$TEST]}"
+    echo "Running cli test $TEST with $SHELL..."
+
+    if ! command -v "$SHELL" >/dev/null 2>&1; then
+        echo "ERROR: Shell $SHELL not found!"
+        exit 1
+    fi
+
+    if "$SHELL" -c "set -e; $TEST"; then
+        echo -e "PASSED: $TEST"
+    else
+        echo -e "FAILED: $TEST (exit code $?)"
+        all_tests_passed=false
+    fi
+done
+
+
+if $all_tests_passed; then
+    echo "All cli tests passed!"
+else
+    echo "Some cli tests failed."
+    exit 1
+fi
+
 
